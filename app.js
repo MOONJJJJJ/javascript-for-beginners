31de35b9347d2d2911db6a00c25cd84b1f881586
MOONJJJJJ <yangmoonjj5@gmail.com>
2023-08-06 00:34:48 +0900

    4.0

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi")
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form Button");
+
+function onLoginBtnClick(){
+    alert("click!!");
+    loginInput.value = "WOW";
+}
+
+loginButton.addEventListener("click",onLoginBtnClick);
