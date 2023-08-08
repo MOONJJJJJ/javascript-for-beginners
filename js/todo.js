396b409d1696929a88e2f5cdb9ee3aa40244c5df
MOONJJJJJ <yangmoonjj5@gmail.com>
2023-08-08 21:24:58 +0900

    7.1 Adding To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText=newTodo;
+    toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event){
     event.preventDefault();
@@ -9,6 +16,7 @@ function handleToDoSubmit(event){
     console.log(toDoInput.value);
     toDoInput.value="";
     console.log(newTodo,toDoInput.value);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
